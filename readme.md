33d025ef3f8fd654bc03abb487f51b17b65b45ce
E-Bor <petr.islankin@mail.ru>
2025-01-11 18:09:11 +0700

    change tests

---
+++ readme.md
@@ -0,0 +1,2 @@
+## About tests
+In order not to launch everything manually, I made a positive test close to real use. Also tested were different cases of invalid ports.
--- server/server_test.go
+++ server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -14,39 +15,131 @@ func TestServerReadWrite(t *testing.T) {
 	srv.Start()
 	defer srv.Stop()
 
-	for i := 1; i <= numIn; i++ {
-		val, err := srv.Read(i)
-		if err != nil {
-			t.Fatalf(
-				"Error while reading IN-port #%d: %v",
-				i,
-				err,
+	t.Run(
+		"Positive cases",
+		func(t *testing.T) {
+			t.Run(
+				"Read from valid IN-ports",
+				func(t *testing.T) {
+					for i := 1; i <= numIn; i++ {
+						val, err := srv.Read(i)
+						if err != nil {
+							t.Fatalf(
+								"Error while reading IN-port #%d: %v",
+								i,
+								err,
+							)
+						}
+						t.Logf(
+							"Read from IN-port #%d: %d",
+							i,
+							val,
+						)
+					}
+				},
 			)
-		}
-		t.Logf(
-			"Read from IN-port #%d: %d",
-			i,
-			val,
-		)
+
+			t.Run(
+				"Write to valid OUT-ports",
+				func(t *testing.T) {
+					for i := 1; i <= numOut; i++ {
+						transactionID := i
+						value := i % 2
+
+						err := srv.Write(
+							i,
+							transactionID,
+							value,
+						)
+						if err != nil {
+							t.Fatalf(
+								"Error while writing to OUT-port #%d: %v",
+								i,
+								err,
+							)
+						}
+						t.Logf(
+							"Wrote to OUT-port #%d: transactionID=%d, value=%d",
+							i,
+							transactionID,
+							value,
+						)
+					}
+				},
+			)
+		},
+	)
+	invalidPorts := []int{
+		0,
+		-1,
+		numIn + 1,
+		numIn + 10,
 	}
 
-	for i := 1; i <= numOut; i++ {
-		if err := srv.Write(
-			i,
-			i,
-			i%2,
-		); err != nil {
-			t.Fatalf(
-				"error OUT-порт #%d: %v",
-				i,
-				err,
+	t.Run(
+		"Negative cases",
+		func(t *testing.T) {
+			t.Run(
+				"Read with invalid port numbers",
+				func(t *testing.T) {
+					for _, portNum := range invalidPorts {
+						t.Run(
+							fmt.Sprintf(
+								"IN-port #%d",
+								portNum,
+							),
+							func(t *testing.T) {
+								_, err := srv.Read(portNum)
+								if err == nil {
+									t.Fatalf(
+										"Expected error for IN-port #%d, but got nil",
+										portNum,
+									)
+								}
+								t.Logf(
+									"Got expected error for IN-port #%d: %v",
+									portNum,
+									err,
+								)
+							},
+						)
+					}
+				},
 			)
-		}
-		t.Logf(
-			"write to OUT-port #%d: transactionID=%d, value=%d",
-			i,
-			i,
-			i%2,
-		)
-	}
+
+			t.Run(
+				"Write with invalid port numbers",
+				func(t *testing.T) {
+					for _, portNum := range invalidPorts {
+						t.Run(
+							fmt.Sprintf(
+								"OUT-port #%d",
+								portNum,
+							),
+							func(t *testing.T) {
+								transactionID := 999
+								value := 1
+								err := srv.Write(
+									portNum,
+									transactionID,
+									value,
+								)
+								if err == nil {
+									t.Fatalf(
+										"Expected error for OUT-port #%d, but got nil",
+										portNum,
+									)
+								}
+								t.Logf(
+									"Got expected error for OUT-port #%d: %v",
+									portNum,
+									err,
+								)
+							},
+						)
+					}
+				},
+			)
+		},
+	)
 }
